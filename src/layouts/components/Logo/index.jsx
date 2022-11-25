142c29b3f3c416d8eaba89ed805aa852a38972b5
xgbnl <745502274@qq.com>
2022-11-25 16:59:27 +0800

    chore: 优化左侧LOGO展开样式

--- src/layouts/components/Logo/index.jsx
+++ src/layouts/components/Logo/index.jsx
@@ -1,11 +1,11 @@
 import './index.scss';
 
-const Logo = ({ collapsed, logo, title }) => {
+const Logo = ({logo, title, collapsed}) => {
 
     return (
         <div className="logo-wrap">
-            <img className={collapsed ? 'collapsedLogo' : ''} src={logo} alt="logo" />
-            {collapsed ? <></> : <h1>{collapsed ? '' : title}</h1>}
+            <img className={collapsed ? 'collapsedLogo' : ''} src={logo} alt="logo"/>
+            <h1 style={{display: collapsed ? 'none' : 'block'}}>{title}</h1>
         </div>
     );
 
