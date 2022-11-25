0fc1bfc242dbeda312d0fda609254c9682d43f0c
xgbnl <745502274@qq.com>
2022-11-25 17:00:51 +0800

    feat: 自定义trigger收缩按钮

---
+++ src/layouts/components/Trigger/index.jsx
@@ -0,0 +1,12 @@
+import {MenuFoldOutlined, MenuUnfoldOutlined} from '@ant-design/icons';
+import './index.scss';
+
+const Trigger = ({collapsed}) => {
+    return (
+            <span className='anticon anticon-left'>
+            {collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>}
+            </span>
+    )
+}
+
+export default Trigger;
