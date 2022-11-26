848e018b32d26215273f63d8ba709a1f9be6e6af
xgbnl <745502274@qq.com>
2022-11-26 09:30:11 +0800

    update: antd依赖升级

---
+++ src/layouts/components/Breadcrumb/index.jsx
@@ -0,0 +1,63 @@
+import {Breadcrumb} from "antd";
+import {useLocation} from "react-router-dom";
+import {findSideBarRoutes} from "@/router/module/permission.jsx";
+import {Link} from 'react-router-dom';
+import {pathJoin} from "@utils/utils.js";
+import './index.scss';
+
+const BreadcrumbItem = Breadcrumb.Item;
+
+// 创建面包屑maps
+const createBreadcrumbMaps = (routes) => {
+    const breadcrumbMaps = {};
+    const find = (routes, parentPath = null) => {
+        for (const route of routes) {
+            if (!route.hidden) {
+                breadcrumbMaps[pathJoin(route.path, parentPath)] = {
+                    label: route.meta?.title,
+                };
+            }
+            if (route.children && route.children.length) {
+                breadcrumbMaps[pathJoin(route.path, parentPath)].isParent = true;
+                find(route.children, pathJoin(route.path, parentPath));
+            }
+        }
+    }
+    find(routes);
+    return breadcrumbMaps;
+}
+
+const CustomBreadcrumb = () => {
+
+    const location = useLocation();
+    const {pathname} = location;
+
+    const breadcrumbMaps = createBreadcrumbMaps(findSideBarRoutes());
+
+    const pathSnippets = pathname.split('/').filter(i => i);
+
+    const extraBreadcrumbItems = pathSnippets.map((_, index) => {
+        const url = `/${pathSnippets.slice(0, index + 1).join('/')}`;
+        return (
+            <BreadcrumbItem key={url}>
+                {
+                    breadcrumbMaps[url].isParent
+                        ? <span>{breadcrumbMaps[url].label}</span>
+                        : <Link to={url}>{breadcrumbMaps[url].label}</Link>
+                }
+            </BreadcrumbItem>
+        );
+    })
+
+    const breadcrumbItems = [
+        <BreadcrumbItem key='home'>
+            <Link to='/'>首页</Link>
+        </BreadcrumbItem>
+    ].concat(extraBreadcrumbItems);
+
+    return (
+        <Breadcrumb>{breadcrumbItems}</Breadcrumb>
+    );
+}
+
+export default CustomBreadcrumb;
