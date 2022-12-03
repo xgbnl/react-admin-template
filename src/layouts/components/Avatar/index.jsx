0712f493c6252932ab333665187673734736d31e
xgbnl <745502274@qq.com>
2022-12-03 19:59:14 +0800

    feat: avatar drown add icon.

--- src/layouts/components/Avatar/index.jsx
+++ src/layouts/components/Avatar/index.jsx
@@ -1,6 +1,7 @@
-import {Dropdown, Avatar, Image} from "antd";
+import {Dropdown, Avatar, Image, Space} from "antd";
 import {useAppDispatch} from "@/app/hooks.js";
 import {logoutAsync} from "@/app/reducers/user/UserReducer.js";
+import {UserOutlined,SettingOutlined,LogoutOutlined } from '@ant-design/icons'
 
 const CustomAvatar = ({avatar}) => {
 
@@ -12,18 +13,33 @@ const CustomAvatar = ({avatar}) => {
     const items = [
         {
             key: '0',
-            label:  <a >个人中心</a>,
+            label: (
+                <Space>
+                    <UserOutlined/>
+                    <span>个人中心</span>
+                </Space>
+            ),
         },
         {
             key: '1',
-            label:  <a >个人设置</a>,
+            label: (
+                <Space>
+                    <SettingOutlined />
+                    <span>个人设置</span>
+                </Space>
+            ),
         },
         {
             type: 'divider',
         },
         {
             key: '2',
-            label:  <a onClick={handelLogout}>退出登录</a>,
+            label: (
+                <Space>
+                    <LogoutOutlined />
+                    <span onClick={handelLogout}>退出登录</span>
+                </Space>
+            ),
         },
     ];
 
