c08fe7218b65df5f3ccf9d110b07e8c6323f3ab8
xgbnl <745502274@qq.com>
2022-12-07 18:00:44 +0800

    feat: add global method.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -17,3 +17,25 @@ export function createDropdownItems(items) {
 
     return result;
 }
+
+/**
+ * 获取数组对象索引
+ * @param haystack 数组
+ * @param property 数组对象属性
+ * @param needle 值
+ * @returns {*}
+ */
+export function getArrayElementIndex(haystack, property, needle) {
+    return haystack.indexOf(haystack.filter(item => item[property] === needle)[0])
+}
+
+/**
+ * 将元素推到数组第一位
+ * @param haystack
+ * @param index
+ * @returns {*}
+ */
+export function pushToShift(haystack, index) {
+    haystack.unshift(haystack.splice(index, 1)[0]);
+    return haystack;
+}
