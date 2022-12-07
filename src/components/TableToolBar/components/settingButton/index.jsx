12e93be4cdb2735e0ff023bd30ed53bd9917d704
xgbnl <745502274@qq.com>
2022-12-07 22:13:55 +0800

    fix: 修复动态创建下拉项时“key”不存在问题

--- src/components/TableToolBar/components/settingButton/index.jsx
+++ src/components/TableToolBar/components/settingButton/index.jsx
@@ -23,16 +23,16 @@ const SettingButton = ({columns, setColumns}) => {
     const [color, setColor] = useState('');
     const [readColumns, setReadColumns] = useState(columns);
 
-    const [dropdownStyles, setDropdownStyles] = useState(createStyles(columns));
+    const [dropdownStyles, setDropdownStyles] = useState(cloneDeep(createStyles(columns)));
+
+    useEffect(() => {
+        setReadColumns(readColumns);
+    }, [columns])
 
     const onMouseEvent = () => {
         setSpin(!spin);
         setColor(color.length ? '' : '#167fff');
     }
-
-    useEffect(() => {
-        setReadColumns(readColumns);
-    }, [columns])
 
     const handelColumnFixed = (key, fixed) => {
         let cloneColumns = cloneDeep(readColumns);
@@ -48,50 +48,64 @@ const SettingButton = ({columns, setColumns}) => {
     }
 
     const handelDropdownMouse = (index) => {
-        const clone = JSON.parse(JSON.stringify(dropdownStyles));
+        const clone = cloneDeep(dropdownStyles);
         const value = clone[index].visibility;
+
+        console.log('current')
+        console.log(index)
+        console.log('current value')
+        console.log(value)
         clone[index].visibility = value === 'visible' ? 'hidden' : 'visible';
+
+        console.log('change')
+        console.log(clone)
+
+
+        // console.log('before')
+        // console.log(dropdownStyles);
+        // console.log('after');
+        // console.log(clone)
+
         setDropdownStyles(clone);
     }
 
-    const createComponent = (title, column, fixed,Component) => {
-        return (<Tooltip placement="bottom" title={title}>
-            <Component
-                onClick={() => handelColumnFixed(column.dataIndex, fixed)}
-                style={dropdownStyles[column.dataIndex]}
-            />
-        </Tooltip>)
-    }
-
-    const render = (column) => {
-
-        if (column.fixed) {
-            if (column.fixed === 'left') {
-                return[
-                    createComponent('取消固定',column,'left',VerticalAlignMiddleOutlined),
-                    createComponent('固定在列尾',column,'right',VerticalAlignTopOutlined)
-                ];
-            }
-
-            return[
-                createComponent('固定在列首',column,'left',VerticalAlignMiddleOutlined),
-                createComponent('取消固定',column,'right',VerticalAlignMiddleOutlined)
-            ];
+    // render dropdown items.
+    const renderComponent = (column) => {
+        const make = (title, column, fixed, Icon,key) => {
+            return (<Tooltip placement="bottom" title={title} key={key}>
+                <Icon
+                    onClick={() => handelColumnFixed(column.dataIndex, fixed)}
+                    style={dropdownStyles[column.dataIndex]}
+                />
+            </Tooltip>)
         }
 
-         return [
-             createComponent('固定在列首',column,'left',VerticalAlignTopOutlined),
-             createComponent('固定在列尾',column,'right',VerticalAlignTopOutlined)
-         ];
+        const fixedStatus = {
+            cancelFixed: {title: '取消固定', column: column, fixed: 'left', icon: VerticalAlignMiddleOutlined},
+            leftFixed: {title: '固定在列首', column: column, fixed: 'left', icon: VerticalAlignTopOutlined},
+            rightFixed: {title: '固定在列尾', column: column, fixed: 'right', icon: VerticalAlignBottomOutlined},
+        };
+
+        const factory = (fixedStatus) => {
+            return fixedStatus.map((item,index) => make(item.title, item.column, item.fixed, item.icon,index));
+        }
+
+        if (!column.fixed) {
+            return factory([fixedStatus.leftFixed, fixedStatus.rightFixed]);
+        }
+
+        return column.fixed === 'left'
+            ? factory([fixedStatus.cancelFixed, fixedStatus.rightFixed])
+            : factory([fixedStatus.leftFixed, fixedStatus.cancelFixed]);
     }
 
-    const dropdowns = columns.map(column => (
+    const dropdowns = columns.map((column) => (
         <Space
             onMouseEnter={() => handelDropdownMouse(column.dataIndex)}
             onMouseLeave={() => handelDropdownMouse(column.dataIndex)}
         >
             <span>{column.title}</span>
-            {render(column)}
+            {renderComponent(column)}
         </Space>
     ));
 
