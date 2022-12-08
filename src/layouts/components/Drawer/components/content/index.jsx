080ff1716829a13e3b55bc50aaf6b53a45a998bc
xgbnl <745502274@qq.com>
2022-12-08 11:34:17 +0800

    feat: 抽出内容区域组件

---
+++ src/layouts/components/Drawer/components/content/index.jsx
@@ -0,0 +1,10 @@
+
+const DrawerContent = () => {
+    return (
+        <div className='drawer-content-container'>
+
+        </div>
+    )
+}
+
+export default DrawerContent;
--- src/layouts/components/Drawer/index.jsx
+++ src/layouts/components/Drawer/index.jsx
@@ -1,6 +1,7 @@
 import {SettingFilled, CloseOutlined} from '@ant-design/icons';
 import {Drawer} from "antd";
 import {useState} from "react";
+import DrawerContent from "./components/content/index.jsx";
 import './index.scss';
 
 const AntdDrawer = () => {
@@ -8,19 +9,22 @@ const AntdDrawer = () => {
     const [closable] = useState(false);
     const [drawerVisible] = useState(true);
 
+    const style = {transform: open ? 'unset' : 'translateX(100%)', width: '300px'};
+
     const showDrawer = () => {
         setOpen(!open);
     };
 
     return (
         <Drawer closable={closable}
-                contentWrapperStyle={{transform: open ? 'unset' : 'translateX(100%)', width: '300px'}}
+                contentWrapperStyle={style}
                 placement="right"
                 mask={open}
                 open={drawerVisible}>
             <div className='handel' onClick={showDrawer}>
                 {open ? <CloseOutlined className='handel-icon'/> : <SettingFilled className='handel-icon'/>}
             </div>
+            <DrawerContent/>
         </Drawer>
     );
 }
