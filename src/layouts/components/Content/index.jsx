3c60092835d5c7da48a8d392b86165bb2cef3fcb
xgbnl <745502274@qq.com>
2022-12-09 21:52:57 +0800

    chore: 抽出notification逻辑

--- src/layouts/components/Content/index.jsx
+++ src/layouts/components/Content/index.jsx
@@ -1,31 +1,20 @@
 import {useEffect, useRef} from "react";
 import {Layout} from "antd";
 import Notification from "@/layouts/components/Notification/index.jsx";
-import {useAppSelector, useAppDispatch} from "@/app/hooks.js";
-import {selectUser, setNotification} from "@/app/reducers/user/UserReducer.js";
 import './index.scss'
 
 const {Content} = Layout;
 const CustomContent = ({children}) => {
     const ref = useRef(null);
 
-    const user = useAppSelector(selectUser);
-    const dispatch = useAppDispatch();
-
     useEffect(() => {
-
-        if (user.notification) {
             ref.current.openNotification();
-            dispatch(setNotification(false));
-        }
-
     }, [])
-
 
     return (
         <Content style={{margin: '15px'}}>
             <div className='content'>
-                <Notification ref={ref} userName={user.name}/>
+                <Notification ref={ref}/>
                 {children}
             </div>
         </Content>
