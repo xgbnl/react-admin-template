d7fac7c86c0df711659559e8ba352ae9cbf05965
xgbnl <745502274@qq.com>
2022-12-10 16:37:22 +0800

    feat: menu 页新增全选，删除功能

--- src/pages/permission/menu/index.jsx
+++ src/pages/permission/menu/index.jsx
@@ -2,27 +2,34 @@ import {useEffect, useState} from "react";
 import {Table} from 'antd';
 import TableToolBar from "@components/TableToolBar/index.jsx";
 import useAntDesign from "@/common/useAntDesign.js";
-import Paginate from "@components/paginate/index.jsx";
+import Paginate from "@components/Paginate/index.jsx";
 import {getMenus} from "@api/permission/menu/index.js";
 import columnsData from "./columns.jsx";
 
-// rowSelection objects indicates the need for row selection
-const rowSelection = {
-    onChange: (selectedRowKeys, selectedRows) => {
-        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-    },
-    onSelect: (record, selected, selectedRows) => {
-        console.log(record, selected, selectedRows);
-    },
-    onSelectAll: (selected, selectedRows, changeRows) => {
-        console.log(selected, selectedRows, changeRows);
-    },
-};
 const Menu = () => {
 
+    const [selectRows, setSelectRows] = useState(0);
+    const [selectAll, setSelectAll] = useState(false);
+
+    const [rowData, setRowData] = useState([]);
+
+    const rowSelection = {
+        onSelect: (record, selected, selectedRows) => {
+            setSelectRows(selectedRows.length);
+            setRowData(selectedRows);
+        },
+        onSelectAll: (selected, selectedRows) => {
+            setSelectAll(selected);
+            setRowData(selectedRows)
+        },
+    };
+
+    // table tool bar
     const [tableSize, setTableSize] = useState('large');
     const [tableBorder, setTableBorder] = useState(false);
     const [columns, setColumns] = useState(columnsData);
+
+    // request
     const [current, setCurrent] = useState(1);
     const [total, setTotal] = useState(0);
     const [pageSize, setPageSize] = useState(10);
@@ -49,16 +56,29 @@ const Menu = () => {
         getMenuList();
     }
 
+    const onCreate = () => {
+
+    }
+
+    const onBatchDelete = () => {
+        console.log(rowData)
+    }
+
     return (
         <>
             <TableToolBar
-                title='菜单管理'
                 setSize={setTableSize}
                 setBordered={setTableBorder}
                 bordered={tableBorder}
                 columns={columns}
                 setColumns={setColumns}
                 onRefresh={onRefresh}
+                onCreate={onCreate}
+                createPermission='permission.menu.create'
+                deletePermission='permission.menu.delete'
+                selectRows={selectRows}
+                selectAll={selectAll}
+                onBatchDelete={onBatchDelete}
             />
             <Table
                 columns={columns}
