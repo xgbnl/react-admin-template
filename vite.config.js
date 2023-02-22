91b24c8278054a860f68b691762f5d1d7d036ed5
xgbnl <745502274@qq.com>
2023-02-22 15:42:00 +0800

    chore: 变更.env代理内容

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+import {resolve} from 'path';
 import {loadEnv} from 'vite';
 
 // https://vitejs.dev/config/
@@ -31,12 +31,12 @@ export default defineConfig(({command, mode}) => {
         plugins: [react()],
         resolve: {
             alias: {
-                '@': path.resolve(__dirname, 'src'),
-                '@utils': path.resolve(__dirname, 'src/utils'),
-                '@assets': path.resolve(__dirname, 'src/assets'),
-                '@pages': path.resolve(__dirname, 'src/pages'),
-                '@api': path.resolve(__dirname, 'src/api'),
-                '@components': path.resolve(__dirname, 'src/components'),
+                '@': resolve(__dirname, 'src'),
+                '@utils': resolve(__dirname, 'src/utils'),
+                '@assets': resolve(__dirname, 'src/assets'),
+                '@pages': resolve(__dirname, 'src/pages'),
+                '@api': resolve(__dirname, 'src/api'),
+                '@components': resolve(__dirname, 'src/components'),
             }
         },
         base: './',
